bb70b6ae60ad3c93f722303a9a1a08a8621f388c
Fernando Generato <generatof@gmail.com>
2022-08-26 10:27:40 -0300

    url impl

---
+++ url/repository_memory.go
@@ -0,0 +1,44 @@
+package url
+
+type repositoryMemory struct {
+	urls   map[string]*Url
+	clicks map[string]int
+}
+
+func NewMemoryRepository() *repositoryMemory {
+	return &repositoryMemory{
+		make(map[string]*Url),
+		make(map[string]int),
+	}
+}
+
+func (r *repositoryMemory) HasId(id string) bool {
+	_, existe := r.urls[id]
+	return existe
+}
+
+func (r *repositoryMemory) FindById(id string) *Url {
+	return r.urls[id]
+}
+
+func (r *repositoryMemory) FindByUrl(url string) *Url {
+	for _, u := range r.urls {
+		if u.Final == url {
+			return u
+		}
+	}
+	return nil
+}
+
+func (r *repositoryMemory) Save(url Url) error {
+	r.urls[url.Id] = &url
+	return nil
+}
+
+func (r *repositoryMemory) RegisterClick(id string) {
+	r.clicks[id] += 1
+}
+
+func (r *repositoryMemory) BuscarClicks(id string) int {
+	return r.clicks[id]
+}
---
+++ url/url.go
@@ -0,0 +1,85 @@
+package url
+
+import (
+	"math/rand"
+	"net/url"
+	"time"
+)
+
+const (
+	size    = 5
+	symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-+"
+)
+
+type Repository interface {
+	HasId(id string) bool
+	FindById(id string) *Url
+	FindByUrl(url string) *Url
+	Save(url Url) error
+	RegisterClick(id string)
+	FindByClick(id string) int
+}
+
+type Url struct {
+	Id      string    `json:"id"`
+	Created time.Time `json:"criacao"`
+	Final   string    `json:"destino"`
+}
+
+type Stats struct {
+	Url    *Url `json:"url"`
+	Clicks int  `json:"clicks"`
+}
+
+var repo Repository
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func ConfigRepository(r Repository) {
+	repo = r
+}
+
+func RegisterClick(id string) {
+	repo.RegisterClick(id)
+}
+
+func FindOrCreateNewUrl(destiny string) (u *Url, new bool, err error) {
+	if u = repo.FindByUrl(destiny); u != nil {
+		return u, false, nil
+	}
+
+	if _, err = url.ParseRequestURI(destiny); err != nil {
+		return nil, false, err
+	}
+
+	url := Url{generateId(), time.Now(), destiny}
+	repo.Save(url)
+	return &url, true, nil
+}
+
+func Find(id string) *Url {
+	return repo.FindById(id)
+}
+
+func (u *Url) Stats() *Stats {
+	clicks := repo.FindByClick(u.Id)
+	return &Stats{u, clicks}
+}
+
+func generateId() string {
+	newId := func() string {
+		id := make([]byte, size, size)
+		for i := range id {
+			id[i] = symbols[rand.Intn(len(symbols))]
+		}
+		return string(id)
+	}
+
+	for {
+		if id := newId(); !repo.HasId(id) {
+			return id
+		}
+	}
+}
